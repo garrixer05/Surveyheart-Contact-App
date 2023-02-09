e7c7523c4d544c3482c8205b31a05ad3c9a53f3b
garrixer05 <aa20494@gmail.com>
2023-02-10 01:19:41 +0530

    fix

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -2,8 +2,6 @@
 const mongoose = require('mongoose');
 
 
-const MONGODB_uri = 'mongodb+srv://garrixer05:ankit123@cluster0.vm0qp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const MONGODB_uri2 = 'mongosh "mongodb+srv://cluster0.vm0qp.mongodb.net/myFirstDatabase" --apiVersion 1 --username garrixer05'
 
 const connectdb = async ()=>{
   mongoose.set('strictQuery', false);
